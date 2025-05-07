218821dc2e2382bc71fc77eb060575b7d61c40d6
Crumbsbit <ant2288644@maricopa.edu>
2025-05-06 21:37:37 -0700

    Final

--- src/main/java/com/example/sorting/SortingUtility.java
+++ src/main/java/com/example/sorting/SortingUtility.java
@@ -1,26 +1,107 @@
 package com.example.sorting;
 
-public class SortingUtility {
+import java.util.Arrays;
+import java.util.List;
+
+public class SortingUtility
+{
 
 
-    public static <T extends Comparable<T>> void gnomeSort(T[] data) {
+    public static <T extends Comparable<T>> void gnomeSort(T[] a)
+    {
 
         // TODO implement Gnome Sort here
+
+        int pos = 0;
+
+        while(pos < a.length)
+        {
+            if(pos == 0 || a[pos].compareTo(a[pos-1]) >= 0)
+            {
+                pos++;
+            }
+            else
+            {
+                T temp = a[pos];
+                a[pos] = a[pos - 1];
+                a[pos - 1] = temp;
+                pos--;
+            }
+        }
     }
 
 
-    public static <T extends Comparable<T>> void cocktailShakerSort(T[] data) {
+    public static <T extends Comparable<T>> void cocktailShakerSort(T[] A)
+    {
 
         // TODO implement Cocktail Shaker Sort here
+
+        boolean swapped = true;
+
+        do
+        {
+            swapped = false;
+
+            for(int i = 0; i < A.length - 1; i++)
+            {
+                if(A[i].compareTo(A[i + 1]) > 0)
+                {
+                    T temp = A[i];
+                    A[i] = A[i + 1];
+                    A[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if(!swapped)
+            {
+                break;
+            }
+
+            swapped = false;
+
+            for(int i = A.length - 2; i >= 0; i--)
+            {
+                if(A[i].compareTo(A[i + 1]) > 0)
+                {
+                    T temp = A[i];
+                    A[i] = A[i + 1];
+                    A[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+        } while(swapped);
     }
 
 
-    public static <T extends Comparable<T>> void shellSort(T[] data) {
+    public static <T extends Comparable<T>> void shellSort(T[] data)
+    {
 
         // TODO implement Shell Sort here
+
+        List<Integer> gaps = Arrays.asList(701, 301, 132, 57, 23, 10, 4, 1);
+        int n = data.length;
+
+        for(int gap : gaps)
+        {
+            for(int i = gap; i < n; i++)
+            {
+                T temp = data[i];
+                int j = i;
+
+                while(j >= gap && data[j - gap].compareTo(temp) > 0)
+                {
+                    data[j] = data[j - gap];
+                    j -= gap;
+                }
+
+                data[j] = temp;
+            }
+        }
     }
 
-    private static <T extends Comparable<T>> void swap(T[] data, int index1, int index2) {
+    private static <T extends Comparable<T>> void swap(T[] data, int index1, int index2)
+    {
 
         T temp = data[index1];
         data[index1] = data[index2];
